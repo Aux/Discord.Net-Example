bf427e7d3dac2451d268b7cb302dc1db11853316
Aux <justin.gentry@auxesis.tv>
2017-01-05 14:03:45 -0600

    Add example of SocketCommandContext and dep map

--- Discord.Net-Example/CommandHandler.cs
+++ Discord.Net-Example/CommandHandler.cs
@@ -14,30 +14,32 @@ namespace Example
 
         public async Task Install(DiscordSocketClient c)
         {
-            _client = c;                                            // Save an instance of the discord client.
-            _cmds = new CommandService();                           // Create a new instance of the commandservice.
+            _client = c;                                                 // Save an instance of the discord client.
+            _cmds = new CommandService();                                // Create a new instance of the commandservice.                              
             
             await _cmds.AddModulesAsync(Assembly.GetEntryAssembly());    // Load all modules from the assembly.
             
-            _client.MessageReceived += HandleCommand;               // Register the messagereceived event to handle commands.
+            _client.MessageReceived += HandleCommand;                    // Register the messagereceived event to handle commands.
         }
 
         private async Task HandleCommand(SocketMessage s)
         {
-            var msg = s as SocketUserMessage;                       
-            if (msg != null)                                        // Check if the received message is from a user.
-            {
-                var context = new CommandContext(_client, msg);     // Create a new command context.
+            var msg = s as SocketUserMessage;
+            if (msg == null)                                    // Check if the received message is from a user.
+                return;
 
-                int argPos = 0;                                     // Check if the message has either a string or mention prefix.
-                if (msg.HasStringPrefix(Configuration.Load().Prefix, ref argPos) ||
-                    msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
-                {                                                   // Try and execute a command with the given context.
-                    var result = await _cmds.ExecuteAsync(context, argPos);
+            var map = new DependencyMap();                      // Create a new dependecy map.
+            map.Add(_cmds);
+            var context = new SocketCommandContext(_client, msg);     // Create a new command context.
 
-                    if (!result.IsSuccess)                          // If execution failed, reply with the error message.
-                        await context.Channel.SendMessageAsync(result.ToString());
-                }
+            int argPos = 0;                                     // Check if the message has either a string or mention prefix.
+            if (msg.HasStringPrefix(Configuration.Load().Prefix, ref argPos) ||
+                msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {                                                   // Try and execute a command with the given context.
+                var result = await _cmds.ExecuteAsync(context, argPos, map);
+
+                if (!result.IsSuccess)                          // If execution failed, reply with the error message.
+                    await context.Channel.SendMessageAsync(result.ToString());
             }
         }
     }
