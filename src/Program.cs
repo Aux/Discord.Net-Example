19633e4cb7e681283593d78fdfb2869c8b33264d
Aux <justin.gentry@auxesis.tv>
2018-01-21 21:14:49 -0600

    Cleaner init

--- src/Program.cs
+++ src/Program.cs
@@ -1,51 +1,10 @@
-﻿using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Example
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
-            => new Program().StartAsync().GetAwaiter().GetResult();
-
-        private IConfigurationRoot _config;
-
-        public async Task StartAsync()
-        {
-            var builder = new ConfigurationBuilder()    // Begin building the configuration file
-                .SetBasePath(AppContext.BaseDirectory)  // Specify the location of the config
-                .AddJsonFile("_configuration.json");    // Add the configuration file
-            _config = builder.Build();                  // Build the configuration file
-
-            var services = new ServiceCollection()      // Begin building the service provider
-                .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig     // Add the discord client to the service provider
-                {
-                    LogLevel = LogSeverity.Verbose,
-                    MessageCacheSize = 1000     // Tell Discord.Net to cache 1000 messages per channel
-                }))
-                .AddSingleton(new CommandService(new CommandServiceConfig     // Add the command service to the service provider
-                {
-                    DefaultRunMode = RunMode.Async,     // Force all commands to run async
-                    LogLevel = LogSeverity.Verbose
-                }))
-                .AddSingleton<CommandHandler>()     // Add remaining services to the provider
-                .AddSingleton<LoggingService>()     
-                .AddSingleton<StartupService>()
-                .AddSingleton<Random>()             // You get better random with a single instance than by creating a new one every time you need it
-                .AddSingleton(_config);
-
-            var provider = services.BuildServiceProvider();     // Create the service provider
-
-            provider.GetRequiredService<LoggingService>();      // Initialize the logging service, startup service, and command handler
-            await provider.GetRequiredService<StartupService>().StartAsync();
-            provider.GetRequiredService<CommandHandler>();
-
-            await Task.Delay(-1);     // Prevent the application from closing
-        }
+        public static Task Main(string[] args)
+            => Startup.RunAsync(args);
     }
 }
---
+++ src/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(string[] args)
+        {
+            var builder = new ConfigurationBuilder()        // Create a new instance of the config builder
+                .SetBasePath(AppContext.BaseDirectory)      // Specify the default location for the config file
+                .AddJsonFile("_configuration.json");        // Add this (json encoded) file to the configuration
+            Configuration = builder.Build();                // Build the configuration
+        }
+
+        public static async Task RunAsync(string[] args)
+        {
+            var startup = new Startup(args);
+            await startup.RunAsync();
+        }
+
+        public async Task RunAsync()
+        {
+            var services = new ServiceCollection();             // Create a new instance of a service collection
+            ConfigureServices(services);
+
+            var provider = services.BuildServiceProvider();     // Build the service provider
+            provider.GetRequiredService<LoggingService>();      // Start the logging service
+
+            await provider.GetRequiredService<StartupService>().StartAsync();       // Start the startup service
+            await Task.Delay(-1);                               // Keep the program alive
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+            {                                       // Add discord to the collection
+                LogLevel = LogSeverity.Verbose,     // Tell the logger to give Verbose amount of info
+                MessageCacheSize = 1000             // Cache 1,000 messages per channel
+            }))
+            .AddSingleton(new CommandService(new CommandServiceConfig
+            {                                       // Add the command service to the collection
+                LogLevel = LogSeverity.Verbose,     // Tell the logger to give Verbose amount of info
+                DefaultRunMode = RunMode.Async,     // Force all commands to run async by default
+                CaseSensitiveCommands = false       // Ignore case when executing commands
+            }))
+            .AddSingleton<StartupService>()         // Add startupservice to the collection
+            .AddSingleton<LoggingService>()         // Add loggingservice to the collection
+            .AddSingleton<Random>()                 // Add random to the collection
+            .AddSingleton(Configuration);           // Add the configuration to the collection
+        }
+    }
+}
